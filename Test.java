9fed369447819dd658d420c05f03c60a1be8f436
KODE-LIKHITHA <88524218+KODE-LIKHITHA@users.noreply.github.com>
2021-09-11 08:53:35 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,14 @@
+class Test{
+public static void main(String ar[]) {
+byte b=-128;
+double d=1.2;
+boolean bo=true;
+float w=987654321.1234567f;
+float c=6.123456789f;
+System.out.println(w);
+System.out.println(c);
+System.out.println(b);
+System.out.println(d);
+System.out.println(bo);
+}
+}
---
+++ float.java
@@ -0,0 +1,16 @@
+class Test{
+public static void main(String ar[]) {
+byte b=-128;
+double d=1.2;
+boolean bo=true;
+float f1=1;
+float f2=1.2f;
+float f3=1.2345f;
+System.out.println("byte: " +b);
+System.out.println("double: " +d);
+System.out.println("boolean: " +bo);
+System.out.println("float: " +f1);
+System.out.println("float: " +f2);
+System.out.println("float: ' +f3);
+}
+}
